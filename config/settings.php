b0f7dc6a5e13ce33d03f6651c69c5852ba5efc8c
Karel Bemelmans <karel.bemelmans@nordcloud.se>
2016-07-14 11:10:11 +0200

    Removed memcached support since the upstream drupal:7-apache decided to switch from PHP 5.6 to PHP 7.0 and memcached support in PHP 7.0 is still buggy.

--- config/settings.php
+++ config/settings.php
@@ -11,27 +11,6 @@ $databases['default']['default'] = array(
   'prefix'   => !empty($_ENV['DRUPAL_DB_PREFIX']) ? $_ENV['DRUPAL_DB_PREFIX'] : '',
 );
 
-////////////////////////////////////////////////////////////////////////////////
-// Memcache configuration
-$conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
-$conf['lock_inc'] = 'sites/all/modules/contrib/memcache/memcache-lock.inc';
-$conf['memcache_stampede_protection'] = TRUE;
-$conf['cache_default_class'] = 'MemCacheDrupal';
-
-// The 'cache_form' bin must be assigned no non-volatile storage.
-$conf['cache_class_cache_form'] = 'DrupalDatabaseCache';
-
-// Don't bootstrap the database when serving pages from the cache.
-$conf['page_cache_without_database'] = TRUE;
-$conf['page_cache_invoke_hooks'] = FALSE;
-
-// This is a very basic memcached setup where there is one cache in and
-// everything goes into this bin. If you want to have a more specific caching
-// strategy, override the settings.php file in your own container.
-$conf['memcache_servers'] = array(
-  $_ENV['DRUPAL_MEMCACHE_HOST'] . ':' . $_ENV['DRUPAL_MEMCACHE_PORT'] => 'default'
-);
-
 // Enforce SSL if the HTTP_X_FORWARDED_PROTO tell us to.
 if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
   $base_url = 'https://'.$_SERVER['SERVER_NAME'];
